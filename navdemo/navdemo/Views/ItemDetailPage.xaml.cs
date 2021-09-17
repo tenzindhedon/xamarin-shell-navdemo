25ab7312be042b455492b1694604b142b847b09f
tenzin.dhedon <tendhedon@gmail.com>
2021-09-17 08:27:26 -0300

    Initial comit

---
+++ navdemo/navdemo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using navdemo.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace navdemo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ navdemo/navdemo/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using navdemo.Models;
+using navdemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace navdemo.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
